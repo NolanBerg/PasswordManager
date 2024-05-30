c993434b2c20b3d535bf44e2f7a18a8db5bfc612
Nolan Berg <nolanb912@gmail.com>
2024-05-30 00:13:05 -0600

    put passwords in a txt file for personal use

---
+++ password.py
@@ -0,0 +1,42 @@
+RED = '\033[91m'
+GREEN = '\033[92m'
+BLUE = '\033[94m'
+RESET = '\033[0m'
+
+pwd = input("Enter the master password: ")
+
+masterpswd = "1"
+attempts = 0
+while pwd != masterpswd and attempts <= 3:
+    pwd = input(f"\n{RED}The password you entered is incorrect.{RESET}\n\nEnter the master password: ")
+    attempts += 1
+if attempts >= 3:
+    print(f"\n{RED}You have no more password attempts.{RESET}")
+    quit()
+
+def view():
+    with open("password.txt", "r") as f:
+        print("\n")
+        for line in f.readlines():
+            print(line.rstrip())
+
+def add():
+    website = input("\nWhat website is this password for: ")
+    username = input("\nEnter username: ")
+    password = input("\nEnter password: ")
+
+    with open("password.txt", "a") as f:
+        f.write(f"{GREEN}{website} {RESET}\n\n    {BLUE}username:{RESET} {username} {GREEN}|{RESET} {BLUE}password:{RESET} {password}\n\n")
+
+while True:
+    mode = input("\nType view, add, or quit to manage password list: ").strip().lower()
+    if mode == "quit":
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print(f"\n{RED}INVALID ENTRY{RESET}")
+        continue
